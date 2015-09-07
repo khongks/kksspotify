3456f814ec9b13900d295675132934c97feb17e9
Khong Kok Sing <khongks@gmail.com>
2015-09-07 17:28:42 +0800

    Updated to remove the alert(...)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -38,7 +38,6 @@ angular.module('kksspotify.controllers', [])
 	$scope.playlists = [];
 
 	$ionicPlatform.ready(function() {
-		alert('ready ...');
 		var storedToken = window.localStorage.getItem('spotify-token');
 		if(storedToken !== null) {
 			Spotify.setAuthToken(storedToken);
@@ -49,26 +48,14 @@ angular.module('kksspotify.controllers', [])
 	});
 
 	$scope.performLogin = function() {
-		alert('performLogin: ' + clientId);
 		$cordovaOauth.spotify(clientId, ['user-read-private','playlist-read-private']).then(function(result) {
 			window.localStorage.setItem('spotify-token', result.access_token);
 			Spotify.setAuthToken(result.access_token);
 			$scope.updateInfo();		
 		}, function(error) {
 			console.log('ERROR: ' + error);
-			alert('ERROR: ' + error);
 		});
 	};
-
- 	// $scope.performLogin = function() {
-  //     	$cordovaOauth.spotify(clientId, ['user-read-private', 'playlist-read-private']).then(function(result) {
-  //       	window.localStorage.setItem('spotify-token', result.access_token);
-  //       	Spotify.setAuthToken(result.access_token);
-  //       	$scope.updateInfo();
-  //     	}, function(error) {
-  //         	console.log("Error -> " + error);
-  //     	});
-  //   };
 
     $scope.updateInfo = function() {
       Spotify.getCurrentUser().then(function (data) {
